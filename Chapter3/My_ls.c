8425ebbb9ef2adb85ba7dfa4cf2eda76b0525146
mnikn <329121948@qq.com>
2015-12-18 05:23:35 -0800

    modified ls

--- Chapter3/My_ls.c
+++ Chapter3/My_ls.c
@@ -1,6 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+
+char* gid_to_name(gid_t gid)
+{
+    struct group *getgrgid(),*grp_ptr;
+    static char numstr[10];
+
+    if((grp_ptr = getgrgid(gid)) == NULL){
+        sprintf(numstr,"%d",gid);
+        return numstr;
+    }
+    else return grp_ptr->gr_name;
+}
+
+char* uid_to_name(uid_t uid)
+{
+    struct passwd *getpwuid(),*pw_ptr;
+    static char numstr[10];
+
+    if((pw_ptr = getpwuid(uid)) == NULL){
+        sprintf(numstr,"%d",uid);
+        return numstr;
+    }
+    else return pw_ptr->pw_name;
+}
+
+void mode_to_letters(int mode,char str[])
+{
+    strcpy(str,"----------");
+    if(S_ISDIR(mode)) str[0] = 'd';
+    if(S_ISCHR(mode)) str[0] = 'c';
+    if(S_ISBLK(mode)) str[0] = 'b';
+
+    if(mode & S_IRUSR) str[1] = 'r';
+    if(mode & S_IWUSR) str[2] = 'w';
+    if(mode & S_IXUSR) str[3] = 'x';
+
+    if(mode & S_IRGRP) str[4] = 'r';
+    if(mode & S_IWGRP) str[5] = 'w';
+    if(mode & S_IXGRP) str[6] = 'x';
+
+    if(mode & S_IROTH) str[7] = 'r';
+    if(mode & S_IWOTH) str[8] = 'w';
+    if(mode & S_IXOTH) str[9] = 'x';
+}
+
+void show_file_info(char *file_name,struct stat *info_p)
+{
+    char *ctime(),mode_str[11];
+    mode_to_letters(info_p->st_mode,mode_str);
+
+    printf("%s",mode_str);
+    printf("%4d",(int)info_p->st_nlink);
+    printf("%-8s",uid_to_name(info_p->st_uid));
+    printf("%-8s",gid_to_name(info_p->st_gid));
+    printf("%8ld",(long)info_p->st_size);
+    printf("%.12s",4+ctime(&info_p->st_mtime));
+    printf("%s\n",file_name);
+}
+
+void do_stat(char *file_name)
+{
+    struct stat info;
+    if(stat(file_name,&info) == -1)
+        perror(file_name);
+    else
+        show_file_info(file_name,&info);
+}
 
 void do_ls(char dir_name[])
 {
@@ -11,7 +83,7 @@ void do_ls(char dir_name[])
         exit(1);
     }
     while((direntp = readdir(dir_ptr)) != NULL){
-        printf("%s\n",direntp->d_name);
+        do_stat(direntp->d_name);
     }
     closedir(dir_ptr);
 }
