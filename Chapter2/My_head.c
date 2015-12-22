45acc736aa5c487e999c0eaa47ac4682b4c456e9
mnikn <329121948@qq.com>
2015-12-22 06:22:17 -0800

    modified head

--- Chapter2/My_head.c
+++ Chapter2/My_head.c
@@ -3,33 +3,41 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define OFFSET_SIZE 400
+#define BUFF_SIZE 4096
 #define READ_SIZE sizeof(char)
+
+/*
+*   show 10 lines in the first head.
+* count '\n',if number of '\n' equals 10,return
+*/
+
+void show_head(int);
 
 int main(int argc,char *argv[])
 {
-    if(argc != 2){
-        fputs("Error : you need two args.",stderr);
+    if(argc == 1){
         exit(1);
     }
 
-    int fd = open(argv[1],O_RDONLY);
-    if(fd == -1){
-        perror(argv[1]);
-        exit(1);
+    int cur_rec = 0;
+    while(--argc){
+        int fd = open(* ++argv,O_RDONLY);
+        show_head(fd);
+        close(fd);
     }
-
-    char buf[OFFSET_SIZE];
-
-    read(fd,buf,OFFSET_SIZE);
-    for(int i=0;i<OFFSET_SIZE;++i){
-        if(i>=100 && buf[i] == '\n'){
-            printf("%c",buf[i]);
-            break;
-        }
-        printf("%c",buf[i]);
-    }
-
-    close(fd);
     return 0;
+}
+
+void show_head(int fd)
+{
+    char buff[BUFF_SIZE];
+    int begin_index = 0,cur_index = 0;
+    int line_count = 0;
+    while((cur_index += read(fd,buff,BUFF_SIZE)) && line_count != 10){
+        for(int i = begin_index;i < cur_index && line_count != 10;++i){
+            if(buff[i] == '\n') ++line_count;
+            printf("%c",buff[i]);
+        }
+        begin_index = cur_index;
+    }
 }
